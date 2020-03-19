9dbadd05f4c361b4e9e7bc605856a8a28f453a51
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-03-19 17:10:05 +0100

    Initial commit

---
+++ src/main/java/pl/kukla/krzys/in28minutes/microservice/netflixeurekanamingserver/config/EurekaNamingServerConfiguration.java
@@ -0,0 +1,12 @@
+package pl.kukla.krzys.in28minutes.microservice.netflixeurekanamingserver.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Krzysztof Kukla
+ */
+@Configuration
+@EnableEurekaServer
+public class EurekaNamingServerConfiguration {
+}
---
+++ src/test/java/pl/kukla/krzys/in28minutes/microservice/netflixeurekanamingserver/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kukla.krzys.in28minutes.microservice.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
